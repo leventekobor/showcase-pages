6024dd1bf80134bbc8bb6d24a38f0748980c8eae
leventekobor94 <kobor.levente@tigra.hu>
2020-09-15 21:19:56 +0200

    add login page

---
+++ index.js
@@ -0,0 +1,11 @@
+function mouseoverPassword(obj) {
+    var obj = document.getElementById('password-icon')
+    var passwordInput = document.getElementById('jelszo')
+    passwordInput.type = "text";
+}
+
+function mouseoutPassword(obj) {
+    var obj = document.getElementById('password-icon')
+    var passwordInput = document.getElementById('jelszo')
+    passwordInput.type = "password";
+}
